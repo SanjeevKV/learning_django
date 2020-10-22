529a734be387726fe8fb3b1ed890af35ab4b88f4
Sanjeev K V <sanjeev.vadiraj@gmail.com>
2020-10-22 17:02:49 +0530

    Completed tutorial 3 with some modifications (enhancements) in detail

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
+from . import models
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello World! You are at the poll index.")
+  question_list = models.Question.objects.order_by('-pub_date')[:5]
+  #return HttpResponse(', '.join(question.question_text for question in question_list))
+  #question_list = []
+  return render(request, 'polls/index.html', context = {'question_list': question_list})
+
+def detail(request, question_id):
+  question = get_object_or_404(models.Question, pk = question_id)
+  return render(request, 'polls/detail.html', context={'question': question, 'choices': question.choice_set.all()})
+
+def results(request, question_id):
+  return HttpResponse('Requesting results of question {qid}'.format(qid = question_id))
