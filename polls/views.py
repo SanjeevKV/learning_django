df24f24893d5c7266aaec227b51eb86e7be10295
Sanjeev K V <sanjeev.vadiraj@gmail.com>
2020-10-23 12:26:45 +0530

    4.1: Completed form without generic views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from . import models
 # Create your views here.
@@ -11,7 +12,20 @@ def index(request):
 
 def detail(request, question_id):
   question = get_object_or_404(models.Question, pk = question_id)
-  return render(request, 'polls/detail.html', context={'question': question, 'choices': question.choice_set.all()})
+  return render(request, 'polls/detail.html', context={'question': question})
 
 def results(request, question_id):
-  return HttpResponse('Requesting results of question {qid}'.format(qid = question_id))
+  question = get_object_or_404(models.Question, id = question_id)
+  return render(request, 'polls/results.html', context={'question': question})
+
+def vote(request, pk):
+  question = get_object_or_404(models.Question, id = pk)
+  try:
+    selected_choice = question.choice_set.get(id = request.POST['choice'])
+  except (KeyError, models.Choice.DoesNotExist):
+    error_text = "You didn't select a choice"
+    return render(request, 'polls/detail.html', context={'question': question, 'error_text':error_text})
+
+  selected_choice.votes += 1
+  selected_choice.save()
+  return HttpResponseRedirect(reverse('polls:results',args=(pk,)))
